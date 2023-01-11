96244b1bbc93d90965c64628ef720b12599b874e
MichaelJRossDev <michaeljrossdev@gmail.com>
2023-01-11 10:49:30 +0000

    Set up react router

--- fe-games/src/App.js
+++ fe-games/src/App.js
@@ -5,18 +5,23 @@ import SignInPage from './components/SignInPage';
 
 import './App.css';
 import { useState } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   const [username, setUsername] = useState('DefaultUsername');
 
   return (
-    <div>
-      <Header />
-      <SearchPage />
-      <ReviewPage />
-      <SignInPage />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/review/:review_id" element={<ReviewPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
