0bf98c17f9067ecaf97a8599f65b9899f82c3ddc
MichaelJRossDev <michaeljrossdev@gmail.com>
2023-01-11 12:16:15 +0000

    Populate review page

--- fe-games/src/components/FullReview.jsx
+++ fe-games/src/components/FullReview.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
 const FullReview = () => {
+    const [review, setReview] = useState({});
+    const [comments, setComments] = useState([]);
+    const { review_id } = useParams();
+    const date = new Date(review.created_at);
+
+    fetch(`https://michaels-games.onrender.com/api/reviews/${review_id}`)
+    .then(response => response.json())
+    .then(data => {
+        setReview(data.review);
+    })
+
+    fetch(`https://michaels-games.onrender.com/api/reviews/${review_id}/comments`)
+    .then(response => response.json())
+    .then(data => {
+        setComments(data.comments);
+    })
+
     return (
-        <div>
-            <p>FullReview</p>
+        <div className="FullReview">
+            <Link to="/" ><button>🏠</button></Link>
+            <h2>{review.title}</h2>
+            <img className="ReviewImage" src={review.review_img_url} alt="image related to review" />
+            <p className="ReviewBody">{review.review_body}</p>
+            <button>❤️{review.votes}</button><button>💬{review.comment_count}</button>
+            <p>Written by {review.owner}</p>
+            <p>{date.toDateString()}</p>
         </div>
     )
 }
