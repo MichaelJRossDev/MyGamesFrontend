52fe6915ea1191de7e5565c80c544aa82f6b3e20
MichaelJRossDev <michaeljrossdev@gmail.com>
2023-01-12 12:13:15 +0000

    Utilise useEffect when fetching comments

--- fe-games/src/components/CommentList.jsx
+++ fe-games/src/components/CommentList.jsx
@@ -2,15 +2,20 @@ import { useParams } from 'react-router-dom';
 
 import { useState } from "react";
 import CommentCard from "./CommentCard";
+import { useEffect } from 'react';
 
 const CommentList = () => {
     const [sortBy, setSortBy] = useState('');
     const [comments, setComments] = useState([]);
     const { review_id } = useParams();
 
-    fetch(`https://michaels-games.onrender.com/api/reviews/${review_id}/comments`)
-    .then(response => response.json())
-    .then(data => setComments(data.comments))
+    useEffect(() => {
+        fetch(`https://michaels-games.onrender.com/api/reviews/${review_id}/comments`)
+        .then(response => response.json())
+        .then(data => setComments(data.comments))
+    }, [sortBy, comments]
+    )
+
 
 
     return (
